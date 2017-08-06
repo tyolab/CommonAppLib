a5f24240f14d6d8445f0ae6c9b7edf01eb6cffa6
tyodev <deve@tyo.com.au>
2017-08-06 11:48:35 +1000

    add sources that were left out from the previous commit

---
+++ src/main/java/au/com/tyo/app/CommonFragmentList.java
@@ -0,0 +1,51 @@
+package au.com.tyo.app;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+/**
+ *
+ */
+
+public class CommonFragmentList extends CommonFragment {
+
+    private ListView listView;
+
+    private ViewGroup extraViewTopContainer;
+
+    private ViewGroup extraViewBottomContainer;
+
+    public ListView getListView() {
+        return listView;
+    }
+
+    @Override
+    protected void onFragmentAttach(Context context) {
+        super.onFragmentAttach(context);
+
+        setContentViewResId(R.layout.frame_list);
+        // setPage(new PageCommonList((Controller) CommonApp.getInstance(), getActivity()));
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        listView = (ListView) view.findViewById(R.id.list);
+        extraViewBottomContainer = (ViewGroup) view.findViewById(R.id.list_extra_bottom);
+        extraViewTopContainer = (ViewGroup) view.findViewById(R.id.list_extra_top);
+    }
+
+    public void addExtraTopView(int resId) {
+        extraViewTopContainer.addView(LayoutInflater.from(getActivity()).inflate(resId, null));
+    }
+
+    public void addExtraBottomView(int resId) {
+        extraViewBottomContainer.addView(LayoutInflater.from(getActivity()).inflate(resId, null));
+    }
+}
---
+++ src/main/java/au/com/tyo/app/ui/UIList.java
@@ -0,0 +1,11 @@
+package au.com.tyo.app.ui;
+
+import au.com.tyo.android.adapter.ListViewItemAdapter;
+
+/**
+ * Created by Eric Tang (eric.tang@tyo.com.au) on 31/7/17.
+ */
+
+public interface UIList {
+    ListViewItemAdapter getAdapter();
+}
