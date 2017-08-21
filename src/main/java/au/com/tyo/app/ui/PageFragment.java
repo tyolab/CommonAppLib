c9da50400f283075f458b4dd839cc30f7a776153
tyodev <deve@tyo.com.au>
2017-08-21 14:02:18 +1000

    new classes

---
+++ src/main/java/au/com/tyo/app/ui/PageFragment.java
@@ -0,0 +1,14 @@
+package au.com.tyo.app.ui;
+
+/**
+ * Created by Eric Tang (eric.tang@tyo.com.au) on 15/8/17.
+ */
+
+public class PageFragment implements UIEntity {
+
+    @Override
+    public void onActivityStart() {
+
+    }
+
+}
---
+++ src/main/java/au/com/tyo/app/ui/UIEntity.java
@@ -0,0 +1,11 @@
+package au.com.tyo.app.ui;
+
+/**
+ * Created by Eric Tang (eric.tang@tyo.com.au) on 15/8/17.
+ */
+
+public interface UIEntity {
+
+    void onActivityStart();
+
+}
