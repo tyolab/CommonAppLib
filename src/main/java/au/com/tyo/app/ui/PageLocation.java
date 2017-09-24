92a41536289a68b84a4d71602540755b19cf951f
tyodev <deve@tyo.com.au>
2017-09-24 15:22:17 +1000

    added locations distance compuation

--- src/main/java/au/com/tyo/app/ui/PageLocation.java
+++ src/main/java/au/com/tyo/app/ui/PageLocation.java
@@ -23,14 +23,6 @@ import au.com.tyo.app.R;
 
 public class PageLocation extends Page {
 
-    private LocationManager locationManager;
-
-    private List<Location> locations;
-
-    /**
-     *
-     */
-    private Location lastKnownLocation;
 
     /**
      * @param controller
@@ -39,63 +31,6 @@ public class PageLocation extends Page {
     public PageLocation(Controller controller, Activity context) {
         super(controller, context);
 
-        locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
-        locations = new ArrayList<>();
     }
 
-    private boolean checkLocation() {
-        if(!isLocationEnabled())
-            showAlert();
-        return isLocationEnabled();
-    }
-
-    private void showAlert() {
-        final AlertDialog.Builder builder = DialogFactory.createDialogBuilder(
-                getActivity(),
-                R.style.CommonAlertDialog_Light,
-                getActivity().getResources().getString(R.string.enable_location_title),
-                getActivity().getResources().getString(R.string.enable_location_message),
-                null,
-                null);
-
-        builder.setPositiveButton("Location Settings", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
-                        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                        startActivity(getActivity(), intent);
-                    }
-                })
-                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
-                    }
-                });
-
-        final AlertDialog dialog = builder.create();
-        dialog.show();
-    }
-
-    private boolean isLocationEnabled() {
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
-                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-    }
-
-    public void requestGPSLocationUpdates(LocationListener locationListener) {
-        requestLocationUpdates(LocationManager.GPS_PROVIDER, locationListener);
-    }
-
-    public void requestLocationUpdates(String provider, LocationListener locationListener) {
-        locationManager.requestLocationUpdates(
-                provider, 5 * 60 * 1000 /* 5 minutes */, 10, locationListener);
-    }
-
-    public void startTracking(String provider, LocationListener locationListener) {
-        if (null == lastKnownLocation)
-            lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        if (null == lastKnownLocation)
-            lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-
-        if (null != locationListener)
-            requestLocationUpdates(provider, locationListener);
-    }
 }
